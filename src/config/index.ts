a92c165f1c1e23b2c80e4dcbad4d0bad97f1fc0d
Ridwan Ikhsan <ridwan@oneyel.com>
2021-10-24 16:29:49 +0700

    Add config file

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-shadow
+enum Config {
+  APP_TITLE = 'Library Bookshelf',
+  STORAGE_KEY = 'BOOKSHELF_APP',
+  UNCOMPLETED_LIST_BOOK = 'uncompleted',
+  COMPLETED_LIST_BOOK = 'completed',
+}
+
+export default Config;
