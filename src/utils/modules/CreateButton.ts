e49f7e4d7421ba4514637ec485afa2fa6dd11042
Ridwan Ikhsan <ridwan@oneyel.com>
2021-10-24 16:42:57 +0700

    Add function to Create Action Button

---
+++ src/utils/modules/CreateButton.ts
@@ -0,0 +1,17 @@
+const createButton = (
+  buttonClass: string,
+  buttonAttribute: string,
+  eventListener: Function
+) => {
+  const button = document.createElement('button');
+
+  button.classList.add(buttonClass);
+  button.setAttribute('title', buttonAttribute);
+  button.addEventListener('click', (event) => {
+    eventListener(event);
+  });
+
+  return button;
+};
+
+export default createButton;
