81fca4ad0d7bbcca85f46b240a12887dce6d3c46
Ridwan Ikhsan <ridwan@oneyel.com>
2021-10-24 16:34:29 +0700

    Add Function Register Service Worker

--- src/utils/RegisterSW.ts
+++ src/utils/RegisterSW.ts
@@ -1,14 +1,11 @@
-/* eslint-disable no-console */
 const registerSW = async (): Promise<void> => {
   if ('serviceWorker' in navigator) {
     try {
       await navigator.serviceWorker.register('./service-worker.js');
-      console.log('Service worker registration success');
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
   }
-  console.log('Service worker not Support in this Browser');
 };
 
 export default registerSW;
