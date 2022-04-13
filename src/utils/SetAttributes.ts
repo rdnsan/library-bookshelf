98980aa4bf4d1729e884c393ab7ec0aac46b19e7
Ridwan Ikhsan <ridwan.jsx@gmail.com>
2022-04-14 04:44:35 +0700

    Remove unused files

--- src/utils/SetAttributes.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable guard-for-in */
-// prettier-ignore
-const setAttributes = (element: HTMLMetaElement | HTMLLinkElement, attribute: any): void => {
-  for (const key in attribute) {
-    element.setAttribute(key, attribute[key]);
-  }
-};
-
-export default setAttributes;
